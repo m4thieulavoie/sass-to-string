3157ca9d337a297795647d61e581b19a1d1260b0
Mathieu Lavoie <mathieulavoie94@gmail.com>
2020-12-28 14:28:25 -0500

    feat: clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: './demo/index.js',
+  entry: "./src/demo/index.ts",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
   module: {
     rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
         {
             test: /\.styles.scss$/,
             exclude: /node_modules/,
@@ -24,5 +35,11 @@ module.exports = {
             ],
         }
     ],
-  }
+  },
+  devServer: {
+    port: 4000,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
 };
