2fd47b6fc84cbf9e69125bfce78f9f2db585f62e
Mathieu Lavoie <mathieu.lavoie@logmein.com>
2021-05-06 14:55:10 -0400

    new lint package usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  ignorePatterns: ["lib"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "require-jsdoc": 0,
-    "react/no-unescaped-entities": 0,
-    "import/prefer-default-export": 0,
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "no-use-before-define": 0,
-    "class-methods-use-this": 0,
-    "func-names": 0,
-    "no-console": 0,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  extends: require.resolve("@m4thieulavoie/linting"),
 };
