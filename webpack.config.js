774b29069c46b7ef06d403adc7ba633f8d7e6f59
Mathieu Lavoie <mathieu.lavoie@shopify.com>
2021-09-07 19:16:57 -0400

    fix start command

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/demo/index.ts",
+  mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
